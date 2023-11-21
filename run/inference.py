5b993590bf386e19bd966bc46ec6d01a63f44b0d
dbtmddn41 <dbtmddn41@naver.com>
2023-11-21 19:54:32 +0900

    2311210755

--- run/inference.py
+++ run/inference.py
@@ -8,6 +8,7 @@ import pandas as pd
 from pathlib import Path
 import numpy as np
 import polars as pl
+from tensorflow import keras
 from src.dataset import get_dataset
 from src.model import load_model
 from scipy.signal import find_peaks
@@ -16,12 +17,12 @@ from scipy.signal import find_peaks
 def main(cfg: DictConfig):
     model = load_model(cfg)
     test_series = (
-            pl.scan_parquet(Path(cfg.dir.data_dir) / f'test_series.parquet')
+            pl.scan_parquet(Path(cfg.dir.data_dir) / cfg.dir.feature_eng_dir / cfg.series_parquet)
             .collect()
             .to_pandas()
         )
     pred_df = test_predict(cfg, test_series, model)
-    submission = get_submission(pred_df, score_th=cfg.score_th, distance=cfg.distance)
+    submission = get_submission(pred_df, cfg.downsample_rate,score_th=cfg.score_th, distance=cfg.distance)
     submission.write_csv(Path(cfg.dir.output_dir) / "submission.csv")
     
 def test_predict(cfg: DictConfig, series: pd.DataFrame, model)\
@@ -50,7 +51,7 @@ def test_predict(cfg: DictConfig, series: pd.DataFrame, model)\
 
 
 
-def get_submission(preds: pd.DataFrame, score_th: float = 0.02, distance: int = 120) -> pl.DataFrame:
+def get_submission(preds: pd.DataFrame, downsample_rate: int, score_th: float = 0.02, distance: int = 120) -> pl.DataFrame:
     unique_series_ids = pd.unique(preds['series_id'])
     records = []
     for series_id in unique_series_ids:
@@ -65,7 +66,7 @@ def get_submission(preds: pd.DataFrame, score_th: float = 0.02, distance: int = 
                 records.append(
                     {
                         "series_id": series_id,
-                        "step": step,
+                        "step": step * downsample_rate,
                         "event": event_name,
                         "score": score,
                     }
@@ -85,6 +86,21 @@ def get_submission(preds: pd.DataFrame, score_th: float = 0.02, distance: int = 
     row_ids = pl.Series(name="row_id", values=np.arange(len(sub_df)))
     sub_df = sub_df.with_columns(row_ids).select(["row_id", "series_id", "step", "event", "score"])
     return sub_df
+
+
+@keras.saving.register_keras_serializable('CustomBCE')
+class CustomLoss(keras.losses.Loss):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.focal_loss = keras.losses.BinaryFocalCrossentropy()
+    def call(self, y_true, y_pred):
+        onset_loss = 300.*y_true[:, :, 0]*tf.math.log(y_pred[:, :, 0]) + 0.1*(1 - y_true[:, :, 0])*tf.math.log(1 - y_pred[:, :, 0])
+        wakeup_loss = 300.*y_true[:, :, 1]*tf.math.log(y_pred[:, :, 1]) + 0.1*(1 - y_true[:, :, 1])*tf.math.log(1 - y_pred[:, :, 1])
+        awake_loss = y_true[:, :, 2]*tf.math.log(y_pred[:, :, 2]) + (1 - y_true[:, :, 2])*tf.math.log(1 - y_pred[:, :, 2])
+        balanced_bce =  tf.reduce_mean(-onset_loss-wakeup_loss-awake_loss)
+        focal_loss_val = self.focal_loss(y_true, y_pred)
+        return balanced_bce + focal_loss_val
+    
 
 if __name__ == '__main__':
     main()
