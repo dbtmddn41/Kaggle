e9d67ffd44b6ca7e6bec41c3552a351ee53a3f1f
dbtmddn41 <dbtmddn41@naver.com>
2023-12-03 01:39:23 +0900

    add positional encoding

--- src/models/decoder/TransformerDecoder.py
+++ src/models/decoder/TransformerDecoder.py
@@ -34,6 +34,44 @@ class TransformerEncoderBlock(layers.Layer):
         config.update({"embed_dim": self.embed_dim, "dense_dim": self.dense_dim, "num_heads": self.num_heads, 'dropout_rate': self.dropout_rate})
         return config
 
+
+class PositionalEmbedding(tf.keras.layers.Layer):
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+  def build(self, input_shape):
+    super().build(input_shape)
+
+  def call(self, x):
+    input_shape = tf.shape(x)
+    batch_size, seq_len, output_dim = input_shape[0], input_shape[1], input_shape[2]
+    pos_input = tf.tile(tf.expand_dims(tf.keras.backend.arange(0, seq_len), axis = 0), [batch_size, 1])
+    pos_input = tf.keras.backend.cast(pos_input, tf.float32)
+    evens = tf.keras.backend.arange(0, output_dim // 2) * 2
+    odds = tf.keras.backend.arange(0, output_dim // 2) * 2 + 1
+    even_embedding = tf.sin(
+      tf.keras.backend.dot(
+        tf.expand_dims(pos_input, -1),
+        tf.expand_dims(1.0 / tf.pow(
+          10000.0,
+          tf.cast(evens, dtype = tf.float32) / tf.cast(output_dim, dtype = tf.float32)
+          ), 0)
+        )
+      )
+    odd_embedding = tf.cos(
+      tf.keras.backend.dot(
+        tf.expand_dims(pos_input, -1),
+        tf.expand_dims(1.0 / tf.pow(
+          10000.0,
+          tf.cast((odds - 1), dtype = tf.float32) / tf.cast(output_dim, dtype = tf.float32)
+          ), 0)
+        )
+      )
+    embedding = tf.stack([even_embedding, odd_embedding], axis = -1)
+    output = tf.reshape(embedding, [-1, tf.shape(x)[1], output_dim])
+    output += x
+
+    return output
     
 class TransformerEncoder(layers.Layer):
     def __init__(self, n_classes, num_blocks, embed_dim, dense_dim, num_heads, dropout_rate=0.0, **kwargs):
@@ -45,8 +83,7 @@ class TransformerEncoder(layers.Layer):
         self.dense_dim = dense_dim
         self.num_heads = num_heads
         self.dropout_rate = dropout_rate
-
-        # self.pos_embedding = PositionalEmbedding(sequence_len, vocab_size, embed_dim)
+        self.pos_embedding = PositionalEmbedding()
         block_layers = []
         for i in range(num_blocks):
             block_layers.append(TransformerEncoderBlock(embed_dim, dense_dim, num_heads, dropout_rate))
@@ -55,8 +92,8 @@ class TransformerEncoder(layers.Layer):
         self.fc = layers.TimeDistributed(layers.Dense(n_classes, activation='sigmoid'))
 
     def call(self, inputs):
-        # pos_inputs = self.pos_embedding(inputs)
-        encoded = self.block_layers(inputs)
+        pos_inputs = self.pos_embedding(inputs)
+        encoded = self.block_layers(pos_inputs)
         outputs = self.fc(self.dropout(encoded))
         return outputs
 
